94a541edae750d014dd9103a353cea32e7c8b761
wretchwatson <wretchwatson@gmail.com>
2025-12-24 17:09:56 +0300

    feat: implement Pin & Hide toggle for MPV (SUPER+S)

--- .config/hypr/scripts/toggle_mpv_pip.sh
+++ .config/hypr/scripts/toggle_mpv_pip.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# MPV penceresinin ID'sini bul
+# MPV penceresinin ID'sini (address) bul
 WINDOW_ID=$(hyprctl clients -j | jq -r '.[] | select(.class == "mpv") | .address')
 
+# Eğer MPV açık değilse hiçbir şey yapma
 if [ -z "$WINDOW_ID" ]; then
-    # Eğer MPV açık değilse Magic Link'i çağır (Pano boşsa uyarı verir)
-    ~/.config/hypr/scripts/scratch_mpv.sh
+    notify-send "MPV" "Çalışan bir video bulunamadı." -i mpv
     exit 0
 fi
 
@@ -13,11 +13,12 @@ fi
 WORKSPACE=$(hyprctl clients -j | jq -r ".[] | select(.address == \"$WINDOW_ID\") | .workspace.name")
 
 if [[ "$WORKSPACE" == "special:magic" ]]; then
-    # Eğer gizli alandaysa: Aktif workspace'e getir ve pinle
+    # Eğer gizli alandaysa: Mevcut workspace'e getir ve tekrar pinle
     hyprctl dispatch movetoworkspace current,address:$WINDOW_ID
     hyprctl dispatch pin address:$WINDOW_ID
-    hyprctl dispatch focuswindow address:$WINDOW_ID
+    notify-send "PiP" "Video geri getirildi ve pinlendi." -i mpv
 else
-    # Eğer aktif alandaysa: Gizli alana gönder (pin otomatik kalkar)
+    # Eğer görünürdeyse: Gizli alana gönder (ses çalmaya devam eder)
     hyprctl dispatch movetoworkspace special:magic,address:$WINDOW_ID
+    notify-send "PiP" "Video arka plana (gizli alana) alındı." -i mpv
 fi
