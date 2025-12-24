f5d4328cc552a783283ba6150c5631daf64a7482
wretchwatson <wretchwatson@gmail.com>
2025-12-24 17:04:59 +0300

    feat: simplify MPV playback to direct bottom-left PiP

--- .config/hypr/scripts/scratch_mpv.sh
+++ .config/hypr/scripts/scratch_mpv.sh
@@ -3,16 +3,13 @@
 # Panodaki linki al
 URL=$(wl-paste)
 
-# Eğer link boşsa veya geçerli bir URL değilse (basit kontrol) uyarı ver
+# Eğer link boşsa veya geçerli bir URL değilse uyarı ver
 if [[ ! $URL =~ ^http ]]; then
     notify-send "Hata" "Panoda geçerli bir URL bulunamadı!" -i dialog-error
     exit 1
 fi
 
-# MPV'yi arka planda (disown) başlat
-notify-send "MPV Başlatılıyor" "$URL" -i mpv
+# MPV'yi arka planda başlat
+notify-send "PiP Başlatılıyor" "Video sol alt köşeye sabitleniyor..." -i mpv
 mpv "$URL" > /dev/null 2>&1 &
 disown
-
-# Scratchpad alanını otomatik olarak aç (isteğe bağlı, videoyu hemen görmek için)
-hyprctl dispatch togglespecialworkspace magic
